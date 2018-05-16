8a0aeb1209a555c00c6ed668b7c0d28c29fe70f7
Vyankat Sattarwar <vyankat.sattarwar@tomtom.com>
2018-05-16 20:31:57 +0530

    refactored

--- src/main/java/com/vss/linearsearch/CountVowels.java
+++ src/main/java/com/vss/linearsearch/CountVowels.java
@@ -11,23 +11,33 @@ import java.util.Scanner;
  */
 public class CountVowels {
 	
+	/**
+	 * 
+	 * @param str
+	 * Complexity O(n)
+	 */
 	public static void countVowels(String str) {
 		str = str.toLowerCase();
 		int count =0;
 		for (int i = 0; i < str.length(); i++) {
 			char c = str.charAt(i);
-			switch(c) {
-			case 'a':
-			case 'e':
-			case 'i':
-			case 'o':
-			case 'u':
-				count++;
-				break;
-				
-			}
+			count = updateCount(count, c);
 		}
 		System.out.println(count);
+	}
+
+	private static int updateCount(int count, char c) {
+		switch(c) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			count++;
+			break;
+			
+		}
+		return count;
 	}
 	
 	public static void main(String[] args) {
